feeb505c96d66e247b6c5e8e269dd63844ed1f33
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-03 21:30:56 +0000

    Task 9 Re-invention

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-multiple_returns = __import__('8-multiple_returns').multiple_returns
+max_integer = __import__('9-max_integer').max_integer
 
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+n = []
+max_value = max_integer(my_list)
+max_v = max_integer(n)
+print("Max: {}".format(max_value))
+print("Max_V: {}".format(max_v))
 
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+
