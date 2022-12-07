79e67482aac4856456064bc1cb394ac44329bf6f
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-07 19:28:37 +0000

    Task 4...

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+#    diff = []
+#    for i in set_1 and for i in set_2:
+#        if i not in set_2:
+#            diff.append(i)
+#        if j not in set_1:
+#            diff.append(j)
+    return (set_1 - set_2)
