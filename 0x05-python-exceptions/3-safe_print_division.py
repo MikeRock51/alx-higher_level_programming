183d2ace73245e5291313380e7f59eab76222272
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-19 16:11:43 +0000

    Just the little i

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except ZeroDivisionError:
         result = None
     finally:
-        print("inside result: {}".format(result))
-
-    return (result)
+        print("Inside result: {}".format(result))
+        return (result)
