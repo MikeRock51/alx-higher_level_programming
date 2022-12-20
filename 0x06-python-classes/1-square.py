982d69ac1769a3f64b7997f425c60c5ab171755c
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-20 18:41:42 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Represents a square with no functionality"""
+
+
+class Square():
+    """Defines a square by a private size attribute"""
+    def __init__(self, __size):
+        self.__size = __size
