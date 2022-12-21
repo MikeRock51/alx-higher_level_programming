e79bc1ee75bce0aa73c44198e65a78690a780acd
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-21 00:49:49 +0000

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Validates the size of a square and computes it's area"""
+
+
+class Square():
+    """Defines a square and validates it's size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Computes the area of a square by acessing it's size"""
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Prints a square to stdout"""
+    def my_print(self):
+        if self.__size < 1:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
