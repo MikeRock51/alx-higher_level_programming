58c2f1f5f303bd43377c8ba80499ae07a7273d40
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-21 02:38:40 +0000

    Task 6 Exception

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,7 +37,7 @@ class Square():
                 if value[0] >= 0 and value[1] >= 0:
                     self.__position = value
                 else:
-                    raise ValueError("position must be a tuple"
+                    raise TypeError("position must be a tuple"
                                      "of 2 positive integers")
             else:
                 raise TypeError("position must be a tuple of"
