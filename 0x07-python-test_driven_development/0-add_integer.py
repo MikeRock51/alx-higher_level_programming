440afb5d896aebfa4860b92bacde690e365257fa
MikeRock51 <mikerockmusic51@gmail.com>
2022-12-30 19:24:46 +0100

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A module that computes the addition of two numbers"""
+
+
+def add_integer(a, b=98):
+    """Function returns an integer, which is the sum of a and b
+    args:
+        a=> Num 1
+        b=> Num 2
+
+    Returns:
+        The sum of a and b
+
+    Raises:
+        TypeError if a or b is neither a int or a float
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return (int(a) + (b))
