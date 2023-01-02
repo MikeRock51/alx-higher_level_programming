1ca9d2789a139e72daf38015418f43351a4a8908
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-02 21:49:40 +0100

    Documentation T1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,6 @@ class Rectangle:
 
     """A class representation of a Rectangle"""
     def __init__(self, width=0, height=0):
-
         """Initializer of a rectangle class
 
         Args:
@@ -17,6 +16,14 @@ class Rectangle:
             TypeError: If width or height is not an integer
             ValueError: If width or height is less than 0
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
