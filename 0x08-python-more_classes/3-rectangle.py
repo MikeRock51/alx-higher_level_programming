a00809bf69b37efcfde804eff58dd3089ee4b898
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-02 15:54:17 +0100

    Working on Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -34,3 +34,11 @@ class Rectangle:
         # if self.__width == 0 or self.__height == 0:
         #     return (0)
         return (2 * (self.__width + self.__height))
+    
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        height = self.__width
+        while height > 0:
+            print("#" * self.__width)
+            height -= 1
