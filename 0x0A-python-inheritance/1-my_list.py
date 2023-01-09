810e6ae4d9fc47bf8662f68d2c6b397e1924c5a2
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-09 15:36:39 +0100

    Remove init method fro T1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """A class that inherits from list object"""
 
-    def __init__(self, lst=[]):
-        self.lst = lst
-
     def print_sorted(self):
         """Prints a sorted list in ascending order"""
 
