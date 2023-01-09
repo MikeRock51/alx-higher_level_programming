7cab62c13dde854c06c585482c886656a08314d6
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-09 17:35:31 +0100

    Update Task 7 Docstring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,9 +7,11 @@ class BaseGeometry(object):
     and validates integer respectively"""
 
     def area(self):
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates that value is an integer"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
