66bc6ca31e56ec9ec19c3c4aa03bb1416a3be32f
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-10 11:58:58 -0800

    Add solution for Task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains a function that inserts a line
+of text to a file, after each
+line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Appends a given string immediately after
+    the occurrence of a particilar string"""
+
+    new_file = ""
+
+    with open(filename) as f:
+        for line in f:
+            new_file += line
+            if search_string in line:
+                new_file += new_string
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(new_file)
