1aa6995a6130864c465c8bda76fa955088cb6623
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-10 17:17:07 +0100

    Correct Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Module that returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean)
+"""Module that returns the dictionary description
 for JSON serialization of an object"""
 import json
 
