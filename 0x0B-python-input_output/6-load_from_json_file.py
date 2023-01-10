3a7f167b9c82c455a65bd456fa18c38d1ca537c7
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-10 16:42:53 +0100

    Add solution for Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an Object from a JSON file
+
+    Args:
+        filename: File to create from
+    """
+    with open(filename) as f:
+        return (json.load(f))
