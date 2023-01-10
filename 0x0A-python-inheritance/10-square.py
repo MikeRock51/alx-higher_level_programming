ef05fcd6b727b85d677eecd1f302ef3cbeae298a
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-10 06:29:50 +0100

    Add solution for Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """A module containing a class that inherits from BaseGeometry"""
-Square = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(Square):
-    """A rectangle subclass to BaseGeometry"""
-    pass
+class Square(Rectangle):
+    """A Square subclass to Rectangle"""
+    def __init__(self, size):
+        """Initializes a Square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a Square"""
+        return (self.__size ** 2)
