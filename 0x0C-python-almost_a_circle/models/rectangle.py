ff8d4f3b624bc88796522f0ac052ce6fc113a4ea
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-13 02:51:41 +0100

    Overide the __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,7 @@ class Rectangle(Base):
         """Prints a rectangle to standard output with # character"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format\
+            (self.id, self.__x, self.__y, self.__width, self.__height))
