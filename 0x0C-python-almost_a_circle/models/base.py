627706f07c92bdd6ede3c5fc18d18eb6b477b1b0
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-14 11:51:47 +0100

    Update Base.to_json)string condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list-dictionary"""
         json_list = []
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return '"[]"'
 
         return json.dumps(list_dictionaries)
