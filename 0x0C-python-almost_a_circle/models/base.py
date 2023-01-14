c3c1c13490bd8086f8dafd0cea6611c57d335e63
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-14 10:47:57 +0100

    Add to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module contains the base class for this project"""
 
+import json
+
 
 class Base:
     """The base class for our project"""
@@ -13,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '"[]"'
+
+        return json.dumps(list_dictionaries)
