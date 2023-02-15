f289eaf082950ff16f9ea8a0df7d44dc92ef27eb
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-15 20:38:34 +0100

    Task 10 UPDATE

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,6 @@
     -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+-- FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres = tv_show.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
