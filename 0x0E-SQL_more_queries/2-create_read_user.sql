69a9cb9d9aa12b8cd9d802d5cf1795801e42a94e
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-15 18:38:56 +0100

    FLUSH PRIVILEGES

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,3 +7,4 @@
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT PRIVILEGE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
