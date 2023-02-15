3d731e6cab456f796747de8456749dcf5eeb261a
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-15 18:19:13 +0100

    gRANT aLL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,4 +5,4 @@
 
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
