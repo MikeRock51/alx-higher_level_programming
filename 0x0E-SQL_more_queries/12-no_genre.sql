eb615c4505d0c6bd714addbd35afd06a383894b6
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-16 10:35:53 +0100

    Update task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,6 +6,7 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
--- RIGHT JOIN tv_show_genres
--- ON tv_show_genres.show_id = tv_shows.id
+RIGHT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
