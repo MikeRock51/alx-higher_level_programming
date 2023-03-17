be39a9493cc4166cf8d6f3a95e954335d6712f3f
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-17 16:21:25 +0100

    Remove ORDER command

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ from sys import argv
 """A script that lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
-    """Connects and fetches data from the database"""
+    """Connects and fetches data from the database hbtn_0e_0_usa"""
 
     userName = argv[1]
     passWord = argv[2]
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=userName, passwd=passWord, db=dbName)
     cursor = db.cursor()
-    cursor.execute("SELECT * from states ORDER BY states.id ASC")
+    cursor.execute("SELECT * from states")
     states = cursor.fetchall()
 
     for state in states:
