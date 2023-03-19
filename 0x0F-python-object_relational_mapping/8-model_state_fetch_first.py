d4827c17314fa5b85be5f5e338e2798899fd3fc6
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-19 13:06:39 +0100

    Add Task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import session
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    userName = argv[1]
+    passWord = argv[2]
+    dbName = argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{userName}:{passWord}@localhost/{dbName}',
+        pool_pre_ping=True)
+    session = Session(engine)
+
+    state = session.query(State).first()
+
+    print(f"{state.id}: {state.name}")
+
+    session.close()
