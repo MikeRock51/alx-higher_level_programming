c48f935d5343d2812271203d4cd8921d89603ee6
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-19 19:16:37 +0100

    Rmove unwanted comma from 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that creates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    userName = argv[1]
+    passWord = argv[2]
+    dbName = argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{userName}:{passWord}@localhost/{dbName}",
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    city2add = City(name='California')
+    state2add = State(name='San Francisco')
+    state2add.cities.append(city2add)
+    session.add(state2add)
+
+    session.commit()
+    session.close()
