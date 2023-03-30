41cccaaec622940a92ebab927303168d38128731
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-30 17:46:04 +0100

    Fix shellcheck on 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Takes in a URL, sends a request to that URL, and displays the
 # size of the body of the response in bytes
 
-curl -so /dev/null -w "%{size_download}\n" $1
+curl -so /dev/null -w "%{size_download}\n" "$1"
