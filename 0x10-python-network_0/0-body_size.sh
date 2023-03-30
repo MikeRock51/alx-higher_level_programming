e50f32207c8fbd831a4a618a296d2fce2df2287c
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-30 17:48:41 +0100

    Make Task 0 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the
-# size of the body of the response in bytes
-
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
 curl -so /dev/null -w "%{size_download}\n" "$1"
