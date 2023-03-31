17281c8623de634adf607ab8c88756b24a00b9ea
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-31 10:05:03 +0100

    Add Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,5 +7,14 @@ def find_peak(list_of_integers):
     if not list_of_integers or list_of_integers == []:
         return None
 
-    for abs(n) in list_of_integers:
+    i = 1
+    peak = []
+    while i < len(list_of_integers):
+        if list_of_integers[i] > list_of_integers[i -1] and list_of_integers[i] > list_of_integers[i +1]:
+            peak.append(list_of_integers[i])
+        i += 2
 
+    if peak == []:
+        return (max(list_of_integers))
+    else:
+        return (max(peak))
