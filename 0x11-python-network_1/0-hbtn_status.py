4cd496aa6ff200de655300dc78b8d254fa969c41
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-01 06:23:56 +0100

    Prevent script from running when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,14 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 
 
-from urllib.request import urlopen
+if __name__ == "__main__":
+    from urllib.request import urlopen
 
 
-with urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
 
-print("Body response:")
-print(f"    - type: {type(body)}")
-print(f"    - content: {body}")
-print(f"    - utf8 content: {body.decode()}")
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
