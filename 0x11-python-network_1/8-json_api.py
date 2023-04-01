ab6ba6106f1d118bde6459ccdd7592c2a05b8e71
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-01 13:58:02 +0100

    Add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        param = {"q": ""}
+    else:
+        param = {"q": argv[1]}
+
+    res = requests.post('http://0.0.0.0:5000/search_user', data=param)
+
+    try:
+        json_res = res.json()
+        if json_res == {}:
+            print("No result")
+        else:
+            print(f"[{json_res['id']}] {json_res['name']}")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
