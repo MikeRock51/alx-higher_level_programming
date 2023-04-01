8736be87a6533204d68e38822f2c0c84f2525bcd
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-01 06:11:29 +0100

    Add Task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+
+from urllib.request import urlopen
+
+
+with urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+
+print("Body response:")
+print(f"    - type: {type(body)}")
+print(f"    - content: {body}")
+print(f"    - utf-8 content: {body.decode()}")
