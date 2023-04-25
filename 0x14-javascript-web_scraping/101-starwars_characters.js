77e713a1fe5c95902b96faeaad516216d6ac454a
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-25 21:51:10 +0100

    Add task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+/**
+ * Prints all characters of a Star Wars movie
+*/
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+  const movieCharacters = JSON.parse(body).characters;
+  for (let i = 0; i < movieCharacters.length; i++) {
+    fetch(movieCharacters[i])
+      .then(response => response.json())
+      .then(data => console.log(data.name))
+      .catch(error => console.error(error));
+  }
+});
