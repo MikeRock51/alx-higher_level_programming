3396aa9c7eb404856288100f89611b5b466f4e5b
MikeRock51 <mikerockmusic51@gmail.com>
2023-04-25 21:18:06 +0100

    Add task 7 in semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,16 +3,16 @@
  * Prints all characters of a Star Wars movie
 */
 
-const request = require("request");
+const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
 request(url, (error, response, body) => {
-	if (error) throw error;
-	const movieCharacters = JSON.parse(body).characters;
-	for (let i = 0; i < movieCharacters.length; i++) {
-		request(movieCharacters[i], (err, res, bd) => {
-			if (err) throw err;
-			console.log(JSON.parse(bd).name);
-		});
-	}
+  if (error) throw error;
+  const movieCharacters = JSON.parse(body).characters;
+  for (let i = 0; i < movieCharacters.length; i++) {
+    request(movieCharacters[i], (err, res, bd) => {
+      if (err) throw err;
+      console.log(JSON.parse(bd).name);
+    });
+  }
 });
