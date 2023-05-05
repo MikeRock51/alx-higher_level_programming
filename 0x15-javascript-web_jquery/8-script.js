dc71dbd50f1fc974ac6b5bd0423ba16457a3c785
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-05 09:52:20 +0100

    Add tasks 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+
+$.ajax({
+	url: url,
+	success: (response) => {
+		for (title of response.results) {
+			$('ul#list_movies').append(`<li>${title.title}</li>`);
+		}
+	}
+});
