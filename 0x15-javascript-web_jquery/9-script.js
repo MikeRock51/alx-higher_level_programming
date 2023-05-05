15069885c4dce69e3205a7bf97c86740d0bb77bf
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-05 10:17:44 +0100

    task 9 still...

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -2,10 +2,11 @@ const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
 
 $.ajax({
 	url: url,
+	dataType: 'json',
 	crossDomain: true,
 	xhrFields: {
 		withCredentials: true
-	}
+	},
 	success: (response) => {
 		$('div#hello').text(response.hello);
 	},
